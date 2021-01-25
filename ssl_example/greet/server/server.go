7f2384b8211e674e13d2c5a3eb06447ddc11fb35
Dhinesh Rozario <DhineshRozario@gmail.com>
2021-01-25 00:49:18 +0000

    client server communication with ssl certificate

---
+++ ssl_example/greet/server/server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/grpc-go-course/ssl_example/greet/protocolbuffer"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+type server struct{}
+
+func (*server) Greet(ctx context.Context, req *protocolbuffer.GreetRequest) (*protocolbuffer.GreetResponse, error) {
+	log.Printf("Server function Greet() RPC was invoked\nwith request: ==> %v\n", req)
+
+	firstName := req.GetGreeting().GetFirstName()
+
+	result := "Hello " + firstName
+
+	res := &protocolbuffer.GreetResponse{
+		Result: result,
+	}
+
+	return res, nil
+}
+
+func main() {
+	log.Println("Greet Server With SSL!")
+
+	certFile := "./cert/server.crt"
+	keyFile := "./cert/server.key"
+
+	//Not working
+	// certFile := "./old_cert/server.crt"
+	// keyFile := "./old_cert/server.key"
+
+	creds, sslErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+
+	if sslErr != nil {
+		log.Fatalf("Failed to loading the certificates: %v\n", sslErr)
+	}
+	s := grpc.NewServer(grpc.Creds(creds))
+
+	lis, err := net.Listen("tcp", "localhost:50051")
+	// // error handling omitted
+	// s.Serve(lis)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+	}
+	protocolbuffer.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v\n", err)
+	}
+}
